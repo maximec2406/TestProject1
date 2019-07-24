a8461225638c81ec1a3fa8eb04822972c44c3286
maximec2406 <maximec2406@mail.ru>
2019-07-24 17:53:50 +0300

    Add WEB facet Configure tomcate

---
+++ src/main/java/ru/abr/dit/Controllers/MainController.java
@@ -0,0 +1,16 @@
+package ru.abr.dit.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/*")
+    public ModelAndView main() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
--- src/main/java/ru/abr/dit/TestSpring.java
+++ src/main/java/ru/abr/dit/TestSpring.java
@@ -3,20 +3,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
-    public static void main(String args[]){
+//    public static void main(String args[]){
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         //ClassicalMusic music = context.getBean("classicalMusic", ClassicalMusic.class);
 
         //MusicPlayer mp = new MusicPlayer(music);
         //MusicPlayer mp = new MusicPlayer();
         //MusicPlayer mp = new MusicPlayer();
-        MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
-        mp.playMusic();
+//        MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
+//        mp.playMusic();
 
-        context.close();
-    }
+//        context.close();
+//    }
 
 
 }
